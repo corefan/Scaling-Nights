c756ec000a293c9d021397f6d1d8c49b4a0776d3
Antonio Fortino <thoniorf@gmail.com>
2016-05-15 12:43:35 -0700

    Added Strobo light effect scripts

---
+++ Assets/shared/Scripts/StroboLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StroboLight : MonoBehaviour
+{
+	[SerializeField] private Light strobo;
+	public bool isOn = false;
+	public float stroboRate = 0.7f;
+	private float nextStrobo = 0.0f;
+	// Use this for initialization
+	void Start ()
+	{
+		strobo = GetComponent<Light> ();
+		strobo.enabled = isOn;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (strobo != null && Time.time > nextStrobo) {
+			nextStrobo = Time.time + stroboRate;
+			strobo.enabled = !isOn;
+		} else {
+			strobo.enabled = isOn;
+		}
+	}
+}
+
