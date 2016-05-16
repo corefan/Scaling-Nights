de9bbc3408d71eb984383051f87d9ff018141b64
Antonio Fortino <thoniorf@gmail.com>
2016-05-16 15:04:25 +0200

    Added StroboController e Fungus Effect script

--- Assets/shared/Scripts/StroboLight.cs
+++ Assets/shared/Scripts/StroboLight.cs
@@ -1,29 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Light))]
 public class StroboLight : MonoBehaviour
 {
-	[SerializeField] private Light strobo;
+	private Light strobo;
 	public bool isOn = false;
+	public bool activate = false;
 	public float stroboRate = 0.7f;
 	private float nextStrobo = 0.0f;
 	// Use this for initialization
 	void Start ()
 	{
 		strobo = GetComponent<Light> ();
-		strobo.enabled = isOn;
+		strobo.activable = isOn;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		float offset = Random.value;
-		if (strobo != null && Time.time > nextStrobo) {
-			nextStrobo = Time.time + stroboRate * offset;
-			strobo.enabled = !isOn;
-		} else {
-			strobo.enabled = isOn;
+		if (activate) {
+			float offset = Random.value;
+			if (strobo != null && Time.time > nextStrobo) {
+				nextStrobo = Time.time + stroboRate * offset;
+				strobo.activable = !isOn;
+			} else {
+				strobo.activable = isOn;
+			}
 		}
+	}
+
+	public void Activate (float rate)
+	{
+		stroboRate = rate;
+		activate = true;
+	}
+
+	public void Deactivate ()
+	{
+		strobo.activable = false;
+		activate = false;
 	}
 }
 
